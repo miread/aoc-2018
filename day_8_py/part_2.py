6a9ab78e048bc5282077524aea16739bc9817437
miread <miranda.evalyn@gmail.com>
2019-04-13 18:45:35 -0500

    Day 8 complete

--- day_8_py/part_2.py
+++ day_8_py/part_2.py
@@ -1,5 +1,4 @@
 idx = 0
-meta_sum = 0
 
 with open("input.txt", "r") as f:
     data = f.read().replace("\n", "").split(" ")
@@ -11,14 +10,24 @@ def scroller():
     return value
 
 def read_nodes():
-    global meta_sum
     num_children = scroller()
     num_metadata = scroller()
     children = [read_nodes() for x in range(0, int(num_children))]
     metadata = [scroller() for x in range(0, int(num_metadata))]
-    for item in metadata:
-        meta_sum += int(item)
     return [num_children, num_metadata, children, metadata]
 
-print(read_nodes())
-print(meta_sum)
+def sum_node(node):
+    if len(node[2]) > 0:
+        total_sum = 0
+        for entry in node[3]:
+            if int(entry) <= len(node[2]) and int(entry) != 0:
+                total_sum += sum_node(node[2][int(entry) - 1])
+    else:
+        total_sum = 0
+        for entry in node[3]:
+            total_sum += int(entry)
+    return total_sum
+
+result = read_nodes()
+
+print(sum_node(result))
